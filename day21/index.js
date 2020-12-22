4c0ff5a9b2ce25c017c930d005323a4159b251ab
Simeunovic Simo <simeunovics@gmail.com>
2020-12-22 16:47:22 +0100

    WIp

--- day21/index.js
+++ day21/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-const input = fs.readFileSync("./input.txt").toString();
+const _ = require("lodash");
+const input = fs.readFileSync(__dirname + "/input.txt").toString();
 
 class Parser {
   constructor(inputString) {
@@ -30,5 +31,51 @@ class Parser {
   }
 }
 
-class Analyzer {}
-module.exports.Parser = Parser;
+class Analyzer {
+  listSafeIngredients(foodList) {
+    const candidatesMap = new Map();
+    for (const foodObject of foodList) {
+      for (const allergenString of foodObject.allergies) {
+        if (!candidatesMap.has(allergenString))
+          candidatesMap.set(allergenString, []);
+        candidatesMap.get(allergenString).push(foodObject.ingredients);
+      }
+    }
+
+    const allergenSuspectsMap = new Map();
+    candidatesMap.forEach((allIngredientsLists, allergenString) => {
+      allergenSuspectsMap.set(
+        allergenString,
+        _.intersection(...allIngredientsLists)
+      );
+    });
+
+    const discoveredAllergensMap = new Map();
+    discoveredAllergensMap.set("allergens", new Set());
+    discoveredAllergensMap.set("ingredients", new Set());
+    while (
+      allergenSuspectsMap.size > discoveredAllergensMap.get("allergens").size
+    ) {
+      allergenSuspectsMap.forEach(
+        (possibleAllergenIngredientsList, allergenString) => {
+          const remainingIngredientsList = possibleAllergenIngredientsList.filter(
+            (s) => !discoveredAllergensMap.get("ingredients").has(s)
+          );
+          if (remainingIngredientsList.length === 1) {
+            discoveredAllergensMap
+              .get("ingredients")
+              .add(remainingIngredientsList[0]);
+            discoveredAllergensMap.get("allergens").add(allergenString);
+          }
+        }
+      );
+    }
+
+    const safeIngredientsList = foodList
+      .flatMap((f) => f.ingredients)
+      .filter((s) => !discoveredAllergensMap.get("ingredients").has(s));
+
+    return safeIngredientsList;
+  }
+}
+module.exports = { Analyzer, Parser };
--- day21/index.spec.js
+++ day21/index.spec.js
@@ -1,23 +1,31 @@
-const { Parser } = require("./index");
+const { Parser, Analyzer } = require("./index");
 const fs = require("fs");
 describe("main test case", () => {
-  const exampleString = fs.readFileSync("./example.txt").toString();
+  const exampleString = fs.readFileSync(__dirname + "/example.txt").toString();
 
   it("should be able to parse file", () => {
-    const exampleString = fs.readFileSync("./example.txt").toString();
-
     const foods = new Parser(exampleString).parse();
 
     expect(foods.length).toBe(4);
     expect(foods[0].ingredients).toEqual(["mxmxvkd", "kfcds", "sqjhc", "nhms"]);
+    expect(foods[0].allergies).toEqual(["dairy", "fish"]);
+
     expect(foods[1].ingredients).toEqual(["trh", "fvjkl", "sbzzf", "mxmxvkd"]);
+    expect(foods[1].allergies).toEqual(["dairy"]);
+
     expect(foods[2].ingredients).toEqual(["sqjhc", "fvjkl"]);
+    expect(foods[2].allergies).toEqual(["soy"]);
+
     expect(foods[3].ingredients).toEqual(["sqjhc", "mxmxvkd", "sbzzf"]);
+    expect(foods[3].allergies).toEqual(["fish"]);
   });
   it("should be able to analyze food", () => {
     const food = new Parser(exampleString).parse();
 
-    const safeIngredientsList = new Analyzer().listSafeIngreedians(food);
-    except(safeIngredientsList).to;
+    const safeIngredientsList = new Analyzer().listSafeIngredients(food);
+    expect(safeIngredientsList.filter((s) => s === "kfcds").length).toBe(1);
+    expect(safeIngredientsList.filter((s) => s === "nhms").length).toBe(1);
+    expect(safeIngredientsList.filter((s) => s === "sbzzf").length).toBe(2);
+    expect(safeIngredientsList.filter((s) => s === "trh").length).toBe(1);
   });
 });
