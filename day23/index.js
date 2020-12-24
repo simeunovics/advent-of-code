d1f38158d7ebf82137ce41bfd378700fd5e9a058
Simeunovic Simo <simeunovics@gmail.com>
2020-12-24 23:58:45 +0100

    Day 23: Part One

--- day23/index.js
+++ day23/index.js
@@ -1,21 +1,63 @@
 class Game {
-  cups = null;
+  cups = [];
   currentCup = null;
+  destinationCup = 0;
+  maxCup = 0;
+  minCup = 0;
+  selectedCups = [];
   constructor(input) {
     this.cups = input.split("").map((n) => Number.parseInt(n, 10));
     this.currentCup = this.cups[0];
+    this.maxCup = this.cups.slice().sort().reverse()[0];
+    this.minCup = this.cups.slice().sort()[0];
+    this.selectedCups = [];
   }
-  playMove() {
-    return 0;
+  get currentCupIndex() {
+    return this.cups.indexOf(this.currentCup);
+  }
+  get destinationCupIndex() {
+    return this.cups.indexOf(this.destinationCup);
+  }
+  reBallance(pivotIndex) {
+    const leftHandSideItemsList = this.cups.splice(0, pivotIndex);
+    this.cups.push(...leftHandSideItemsList);
+  }
+  play(moves) {
+    for (let i = 1; i <= moves; i++) {
+      this.cups;
+      // Select Cups
+      this.selectedCups = this.cups.splice(this.currentCupIndex + 1, 3);
+
+      // Find destination cup
+      this.destinationCup = this.currentCup - 1;
+      while (true) {
+        if (this.destinationCup < this.minCup) {
+          this.destinationCup = this.maxCup;
+        }
+        if (!this.selectedCups.includes(this.destinationCup)) {
+          break;
+        }
+        this.destinationCup--;
+      }
+
+      // Insert 3 cups in destination cup place
+      this.reBallance(this.destinationCupIndex);
+      this.cups.splice(this.destinationCupIndex + 1, 0, ...this.selectedCups);
+
+      // Pick new current cup
+      this.reBallance(this.currentCupIndex);
+      this.currentCup = this.cups[this.currentCupIndex + 1];
+    }
   }
 
-  // get cups() {
-  //   return [];
-  // }
-
-  // get currentCup() {
-  //   return 0;
-  // }
+  getResult() {
+    const startIndex = this.cups.indexOf(1);
+    const cupsCopyArray = [...this.cups];
+    const leftHandSideItemsArray = cupsCopyArray.splice(0, startIndex);
+    cupsCopyArray.push(...leftHandSideItemsArray);
+    const [first, ...result] = cupsCopyArray;
+    return result.join("");
+  }
 }
 
 module.exports = { Game };
--- day23/index.spec.js
+++ day23/index.spec.js
@@ -1,14 +1,32 @@
 const { Game } = require("./index");
 describe("game loads various cups", () => {
   const cases = [
-    ["1", [1], 1],
-    ["123", [1, 2, 3], 1],
-    ["389125467", [3, 8, 9, 1, 2, 5, 4, 6, 7], 3],
+    ["1", [1], 1, 1],
+    ["123", [1, 2, 3], 1, 3],
+    ["389125467", [3, 8, 9, 1, 2, 5, 4, 6, 7], 3, 9],
   ];
 
-  test.each(cases)("when input is %p", (input, expected, currentCup) => {
-    const game = new Game(input);
-    expect(game.cups).toEqual(expected);
-    expect(game.currentCup).toEqual(currentCup);
+  test.each(cases)(
+    "when input is %p",
+    (input, expected, currentCup, maxCup) => {
+      const game = new Game(input);
+      expect(game.cups).toEqual(expected);
+      expect(game.currentCup).toEqual(currentCup);
+      expect(game.maxCup).toEqual(maxCup);
+    }
+  );
+});
+
+describe("game play", () => {
+  test("it can play game with example input", () => {
+    const game = new Game("389125467");
+    game.play(10);
+    expect(game.getResult()).toBe("92658374");
+  });
+
+  test("it can play game with real input", () => {
+    const game = new Game("219748365");
+    game.play(100);
+    expect(game.getResult()).toBe("35827964");
   });
 });
